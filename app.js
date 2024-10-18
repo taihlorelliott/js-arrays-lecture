fcbb81e3e336cbc5233264f371841e0222fee538
Taihlor Elliott <taihlor.elliott@gmail.com>
2024-10-18 15:07:51 -0400

    objects

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// =================== ARRAYS ===================
 // const myStuff = [2, "oh hai, Mark", true]
 // console.log(myStuff)
 // // console.log(myStuff.length)
@@ -12,4 +13,42 @@
 // console.log(myStuff)
 
 
-const movies = ['Die Hard', 'Alien', 'Barbie']
+// const movies = ['Die Hard', 'Alien', 'Barbie']
+// movies.push('Get Out')
+//     for(let movie of movies){
+//         // console.log(movie)
+//         console.log(`I love ${movie}`)
+//     }
+
+// const movies = ['Die Hard', 'Alien', 'Barbie']
+
+// // for (let num = 0; num < 3; num++){
+// //     console.log(num)
+// // }
+
+// for(let num = 0; num < 3; num++){
+//     console.log(num)
+//     console.log(movies[0])
+
+// }
+
+// for(let currentIndex = 0; currentIndex < 3; currentIndex++){
+//     console.log(movies[currentIndex])
+// }
+
+
+// =============== OBJECTS ==================
+const me = { 
+    firstName: 'Matt', 
+    age: 44, 
+    hadHome: true 
+}
+console.log(me)
+// console.log(me.firstName)
+me.firstName = "Matthew"
+me.lastName = 'Hutungton'
+console.log(me)
+
+delete me.firstName
+console.log(me)
+
