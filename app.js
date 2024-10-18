4103b579eeda9f50950b9eb6fde67006d0584383
Taihlor Elliott <taihlor.elliott@gmail.com>
2024-10-18 13:41:25 -0400

    testing

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('oh hai Mark!')
+// const myStuff = [2, "oh hai, Mark", true]
+// console.log(myStuff)
+// // console.log(myStuff.length)
+
+// // myStuff[0] = 5
+// // console.log(myStuff)
+// // myStuff[1] = 'Hello World!'
+// // console.log(myStuff)
+// myStuff.push("hellllloooo")
+// console.log(myStuff)
+// myStuff.pop()
+// console.log(myStuff)
+
+
+const movies = ['Die Hard', 'Alien', 'Barbie']
